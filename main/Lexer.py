0e3c9c29c198f1b62e36fd6e3d7ccfebe69fbc60
Ziad Rabea <138338114+ZiadRabea@users.noreply.github.com>
2024-02-14 20:25:03 +0200

    lexer update

--- main/Lexer.py
+++ main/Lexer.py
@@ -125,13 +125,13 @@ class Lexer:
         while self.current_char != None and (self.current_char != q or escape_character):
             if escape_character:
                 string += escape_characters.get(self.current_char, self.current_char)
+                escape_character = False
             else:
                 if self.current_char == '\\':
                     escape_character = True
                 else:
                     string += self.current_char
             self.advance()
-            escape_character = False
 
         self.advance()
         return Token(TT_STRING, string, pos_start, self.pos)
